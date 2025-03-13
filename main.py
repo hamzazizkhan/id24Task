07564ac713ee9c1df07d9ee2a6bba86931ada71b
hamza <hamzazizkhan@gmail.com>
2025-03-13 19:57:10 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from crawler import login, search
+
 sessionAndcustom_headers = login()
 session = sessionAndcustom_headers[0]
 custom_headers = sessionAndcustom_headers[1]
-searchQuery = 'microsoft'
-
 
 while True:
     searchQuery = input('\nplease input a company. enter q to quit: ')
     if searchQuery=='q':
         break
     
-    print(searchQuery)
-
     code, data = search(session, custom_headers, searchQuery)
     print('\nstatus code:', code)
     for item in data:
